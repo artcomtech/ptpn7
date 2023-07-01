0b28f12df893bb85a04fe5f00dae9348ce6ea7ea
setio.adinataarianto@outlook.com <setio.adinataarianto@outlook.com>
2023-07-02 01:45:07 +0700

    Commmit Departemen

---
+++ app/Http/Controllers/Admin/DepartemenController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DepartemenController extends Controller
+{
+    public function index(){
+        
+    }
+}
